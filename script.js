3e7c123dc145b45c680c97fbe896056ae82e3a33
Adi0820 <132293606+Adi0820@users.noreply.github.com>
2024-04-24 13:30:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+var hitrn = 0;
+
+function makeBubble(){
+    var clutter = "";
+    for(var i = 1; i <= 102; i++){
+        var rn = Math.floor(Math.random()*10)
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+
+var timer = 3;
+function runTimer(){
+    var timerint = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
+        }
+    },1000)
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10)
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+var score = 0;
+
+function increaseScore(){
+    score+=10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+document.querySelector("#pbtm").addEventListener("click",function(details){
+    var clickedNumber = (Number(details.target.textContent));
+    if(clickedNumber == hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+})
+
+
+makeBubble();
+runTimer();
+getNewHit();
